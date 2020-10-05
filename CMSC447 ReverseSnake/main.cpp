e67fa41f894cf4ac4c11ba3842dd11e1450e6047
Jonathan Haubrich <jhaubrich@umbc.edu>
2020-10-05 11:44:24 -0400

    Prototype player movement, create snake enemy class

--- CMSC447 ReverseSnake/main.cpp
+++ CMSC447 ReverseSnake/main.cpp
@@ -1,11 +1,52 @@
 #include <SFML/Graphics.hpp>
 
+void movePlayer(sf::RectangleShape &shape);
+
+class Snake {
+public:
+	Snake() {
+		_head = _tail = new SnakeNode;
+		_head->segment.setPosition({ 50.f, 50.f });
+		_head->segment.setFillColor(sf::Color::Red);
+	}
+	~Snake()
+	{
+		SnakeNode *tmp;
+		while(_head)
+		{
+			tmp = _head->next;
+			delete _head;
+			_head = tmp;
+		} 
+	}
+
+	void draw(sf::RenderWindow &window)
+	{
+		SnakeNode *tmp = _head;
+		while (tmp)
+		{
+			window.draw(tmp->segment);
+			tmp = _head->next;
+		}
+	}
+
+private:
+	struct SnakeNode {
+		sf::RectangleShape segment{ {10.f, 10.f} };
+		SnakeNode *next{ nullptr };
+	};
+
+	int _length{ 0 };
+	SnakeNode *_head{ nullptr },
+		*_tail{ nullptr };
+};
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Reverse Snake");
+	sf::RectangleShape shape(sf::Vector2f(10.f, 10.f));
 	shape.setFillColor(sf::Color::Green);
-
+	Snake snake;
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -13,12 +54,38 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::KeyPressed)
+				movePlayer(shape);
 		}
 
 		window.clear();
+		snake.draw(window);
 		window.draw(shape);
 		window.display();
 	}
 
 	return 0;
+}
+
+void movePlayer(sf::RectangleShape &shape)
+{
+	sf::Vector2f newPosition = shape.getPosition();
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		newPosition.x -= 10;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		newPosition.x += 10;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		newPosition.y -= 10;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		newPosition.y += 10;
+	}
+
+	shape.setPosition(newPosition);
 }
