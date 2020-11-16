ea8e8651c7af8b3709881c48019fed6212f96d49
JoshTrautwein <51382370+JoshTrautwein@users.noreply.github.com>
2020-11-15 23:46:21 -0500

    Add files via upload
    added textures, primitive snake A.I., wall, and new end screen. Code is still incomplete: need wall collision, need score system, fix game crash when snake collides with self, balance player movement, possible texture for player, etc.

--- CMSC447 ReverseSnake/main.cpp
+++ CMSC447 ReverseSnake/main.cpp
@@ -1,7 +1,9 @@
+#include <array>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/Event.hpp>
 #include <Windows.h>
 
-#define	MOVE_DISTANCE	8
+#define	MOVE_DISTANCE	16
 
 void movePlayer(sf::RectangleShape &shape);
 
@@ -9,26 +11,35 @@ class Snake {
 public:
 	Snake() {
 		_head = _tail = new SnakeNode;
-		_head->_segment.setPosition({ 200.f, 200.f });
+		_head->_segment.setPosition({ 320.f, 320.f });
 	}
 	~Snake()
 	{
 		SnakeNode *tmp;
-		while(_head)
+		while (_head)
 		{
 			tmp = _head->_next;
 			delete _head;
 			_head = tmp;
-		} 
+		}
 	}
 
 	void draw(sf::RenderWindow &window)
 	{
+		sf::Texture snakeHead;
+		snakeHead.loadFromFile("textures\\snakeHead.png");
+		sf::Texture snakeBody;
+		snakeBody.loadFromFile("textures\\snakeBody.png");
 		SnakeNode *tmp = _head;
+		tmp->_segment.setTexture(snakeHead);
 		while (tmp)
 		{
 			window.draw(tmp->_segment);
+			if (tmp->_next == nullptr) {
+				break;
+			}
 			tmp = tmp->_next;
+			tmp->_segment.setTexture(snakeBody);
 		}
 	}
 
@@ -37,19 +48,103 @@ public:
 		_grow = true;
 	}
 
-	void move()
+	void move(sf::Vector2f targetPosition)
 	{
 		int direction = 0;
+		int targetX = 0;
+		int targetY = 0;
 		sf::Vector2f oldPosition = _head->_segment.getPosition(),
 			newPosition = oldPosition,
 			oldTail = _tail->_segment.getPosition(),
 			tempPosition;
+		targetX = abs(targetPosition.x - oldPosition.x);
+		targetY = abs(targetPosition.y - oldPosition.y);
+		if (targetX >= targetY)
+		{
+			if (targetPosition.x < oldPosition.x)
+			{
+				direction = 2;
+			}
+			else {
+				direction = 3;
+			}
+		}
+		else if (targetY >= targetX)
+		{
+			if (targetPosition.y < oldPosition.y)
+			{
+				direction = 0;
+			}
+			else
+			{
+				direction = 1;
+			}
+		}
 		SnakeNode *tmp = nullptr;
 		std::vector<sf::Vector2f> validMoves;
 
 		// Move the head
-		for (int direction = 0; direction < 4; ++direction)
+		newPosition = oldPosition;
+		switch (direction)
 		{
+		case 0: // Up
+			newPosition.y -= MOVE_DISTANCE; // (y axis is flipped. it's weird)
+			break;
+		case 1: // Down
+			newPosition.y += MOVE_DISTANCE;
+			break;
+		case 2: // Left
+			newPosition.x -= MOVE_DISTANCE;
+			break;
+		case 3: // Right
+			newPosition.x += MOVE_DISTANCE;
+			break;
+		}
+		if (positionIsValid(newPosition))
+		{
+			validMoves.push_back(newPosition);
+		}
+
+		if (validMoves.size() == 0)
+		{
+			// Swap head and tail
+			tmp = _tail;
+			_tail->_next = _head->_next;
+			_head->_next->_prev = _tail;
+			_head->_next = nullptr;
+			_head->_prev = _tail->_prev;
+			_tail->_prev->_next = _head;
+			_tail->_prev = nullptr;
+
+			_tail = _head;
+			_head = tmp;
+
+			oldPosition = _head->_segment.getPosition();
+			targetX = abs(targetPosition.x - oldPosition.x);
+			targetY = abs(targetPosition.y - oldPosition.y);
+
+			if (targetX > targetY)
+			{
+				if (targetPosition.x < oldPosition.x)
+				{
+					direction = 2;
+				}
+				else {
+					direction = 3;
+				}
+			}
+			else if (targetY >= targetX)
+			{
+				if (targetPosition.y < oldPosition.y)
+				{
+					direction = 0;
+				}
+				else
+				{
+					direction = 1;
+				}
+			}
+
 			newPosition = oldPosition;
 			switch (direction)
 			{
@@ -69,47 +164,6 @@ public:
 			if (positionIsValid(newPosition))
 			{
 				validMoves.push_back(newPosition);
-			}
-		}
-
-		if (validMoves.size() == 0)
-		{
-			// Swap head and tail
-			tmp = _tail;
-			_tail->_next = _head->_next;
-			_head->_next->_prev = _tail;
-			_head->_next = nullptr;
-			_head->_prev = _tail->_prev;
-			_tail->_prev->_next = _head;
-			_tail->_prev = nullptr;
-			
-			_tail = _head;
-			_head = tmp;
-
-			oldPosition = _head->_segment.getPosition();
-
-			for (int direction = 0; direction < 4; ++direction)
-			{
-				newPosition = oldPosition;
-				switch (direction)
-				{
-				case 0: // Up
-					newPosition.y -= MOVE_DISTANCE; // (y axis is flipped. it's weird)
-					break;
-				case 1: // Down
-					newPosition.y += MOVE_DISTANCE;
-					break;
-				case 2: // Left
-					newPosition.x -= MOVE_DISTANCE;
-					break;
-				case 3: // Right
-					newPosition.x += MOVE_DISTANCE;
-					break;
-				}
-				if (positionIsValid(newPosition))
-				{
-					validMoves.push_back(newPosition);
-				}
 			}
 		}
 
@@ -139,7 +193,7 @@ public:
 		SnakeNode *tmp = _head;
 		while (tmp)
 		{
-			if (tmp->_segment.getPosition() == shape.getPosition())
+			if (tmp->_segment.getGlobalBounds().intersects(shape.getGlobalBounds()))
 			{
 				return true;
 			}
@@ -150,9 +204,9 @@ public:
 
 private:
 	struct SnakeNode {
-		SnakeNode() { _segment.setFillColor(sf::Color::Red); }
-		SnakeNode(sf::Vector2f pos) : SnakeNode() { _segment.setPosition(pos);  }
-		sf::RectangleShape _segment{ {10.f, 10.f} };
+		SnakeNode() {}
+		SnakeNode(sf::Vector2f pos) : SnakeNode() { _segment.setPosition(pos); }
+		sf::Sprite _segment;
 		SnakeNode *_prev{ nullptr },
 			*_next{ nullptr };
 	};
@@ -177,112 +231,310 @@ private:
 	bool _grow{ false };
 };
 
-int main()
+HWND hWnd = GetConsoleWindow();
+ShowWindow(hWnd, SW_HIDE);
+
+sf::RenderWindow window(sf::VideoMode(640, 640), "Reverse Snake");
+sf::RectangleShape player(sf::Vector2f(16.f, 16.f));
+player.setOrigin(-16.f, -16.f);
+player.setFillColor(sf::Color::Green);
+sf::Vector2f playerDirection({ MOVE_DISTANCE, 0.f });
+Snake snake;
+sf::Clock moveClock,
+growthClock;
+sf::Time moveTime,
+growthTime;
+sf::Event event;
+
+//wall textures
+std::array<sf::Sprite, 4> walls;
+sf::Texture wallTxt;
+wallTxt.loadFromFile("textures\\wall.png");
+wallTxt.setRepeated(true);
+for (auto &wall : walls)
 {
-	HWND hWnd = GetConsoleWindow();
-	ShowWindow(hWnd, SW_HIDE);
+	wall.setTexture(wallTxt);
+}
 
-	sf::RenderWindow window(sf::VideoMode(400, 400), "Reverse Snake");
-	sf::RectangleShape player(sf::Vector2f(10.f, 10.f));
-	player.setFillColor(sf::Color::Green);
-	Snake snake;
-	sf::Clock moveClock,
-		growthClock;
-	sf::Time moveTime,
-		growthTime;
-	sf::Event event;
-	
-	sf::Font font;
-	font.loadFromFile("C:\\Windows\\Fonts\\arial.ttf");
-	sf::Text text("Press any key to continue...", font, 16);
-	text.setOrigin(text.getLocalBounds().width / 2.f, text.getLocalBounds().height / 2.f);
-	text.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
+walls[0].setTextureRect({ 0, 0, (int)window.getSize().x, 16 });
+walls[1].setTextureRect({ 0, 0, (int)window.getSize().x, 16 });
+walls[1].setPosition(0, window.getSize().y - 16);
+
+walls[2].setTextureRect({ 0, 0, 16, (int)window.getSize().y });
+walls[3].setTextureRect({ 0, 0, 16, (int)window.getSize().y });
+walls[3].setPosition(window.getSize().x - 16, 0);
+
+// start screen
+sf::Font font;
+font.loadFromFile("C:\\Windows\\Fonts\\arial.ttf");
+sf::Text text("Press any key to continue...", font, 16);
+text.setOrigin(text.getLocalBounds().width / 2.f, text.getLocalBounds().height / 2.f);
+text.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
 
 
-	bool collided = false;
+bool collided = false;
 
-	while (window.isOpen() && window.waitEvent(event))
+while (window.isOpen() && window.waitEvent(event))
+{
+	window.clear();
+	window.draw(text);
+	window.display();
+	if (event.type == sf::Event::KeyPressed)
 	{
-		window.clear();
-		window.draw(text);
-		window.display();
+		break;
+	}
+}
+
+// game loop
+while (window.isOpen())
+{
+	while (window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+			window.close();
 		if (event.type == sf::Event::KeyPressed)
 		{
-			break;
+			movePlayer(player);
 		}
+
 	}
 
-	while (window.isOpen())
+	growthTime = growthClock.getElapsedTime();
+	if (growthTime.asSeconds() >= 3.f)
 	{
-
+		snake.grow();
+		growthClock.restart();
+	}
+	moveTime = moveClock.getElapsedTime();
+	/*
+	if (moveTime.asSeconds() >= 0.3f)
+	{
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-			if (event.type == sf::Event::KeyPressed)
+			else if (event.type == sf::Event::KeyPressed)
 			{
-				movePlayer(player);
+				// Get player direction
+				sf::Vector2f newDirection = playerDirection;
+				switch (event.key.code)
+				{
+				case sf::Keyboard::Up:
+					newDirection = { 0.f, -MOVE_DISTANCE};
+					break;
+				case sf::Keyboard::Down:
+					newDirection = { 0.f, MOVE_DISTANCE};
+					break;
+				case sf::Keyboard::Left:
+					newDirection = { -MOVE_DISTANCE, 0.f };
+					break;
+				case sf::Keyboard::Right:
+					newDirection = { MOVE_DISTANCE, 0.f };
+					break;
+				case sf::Keyboard::W:
+					newDirection = { 0.f, -MOVE_DISTANCE};
+					break;
+				case sf::Keyboard::S:
+					newDirection = { 0.f, MOVE_DISTANCE};
+					break;
+				case sf::Keyboard::A:
+					newDirection = { -MOVE_DISTANCE, 0.f };
+					break;
+				case sf::Keyboard::D:
+					newDirection = { MOVE_DISTANCE, 0.f };
+					break;
+				default:
+					break;
+				}
+				playerDirection = newDirection;
 			}
-
 		}
-
-		growthTime = growthClock.getElapsedTime();
-		if (growthTime.asSeconds() >= 3.f)
-		{
-			snake.grow();
-			growthClock.restart();
-		}
-		moveTime = moveClock.getElapsedTime();
-		if (moveTime.asSeconds() >= 0.3f)
-		{
-			snake.move();
-			moveClock.restart();
-		}
-
-		if (snake.checkCollision(player))
+		movePlayer(player, playerDirection);
+	}
+	*/
+	if (moveTime.asSeconds() >= 0.3f)
+	{
+		sf::Vector2f targetPosition = player.getPosition();
+		snake.move(targetPosition);
+		moveClock.restart();
+	}
+	// check for collision with snake
+	if (snake.checkCollision(player))
+	{
+		collided = true;
+		break;
+	}
+	// check for collision with walls
+	for (auto &wall : walls)
+	{
+		if (wall.getGlobalBounds().intersects(player.getGlobalBounds()))
 		{
 			collided = true;
 			break;
 		}
-
-		window.clear();
-		snake.draw(window);
-		window.draw(player);
-		window.display();
 	}
 
-	if (collided)
+	window.clear();
+	for (auto &wall : walls)
 	{
-		sf::Text gameOver("GAME OVER", font, 32);
-		sf::Text pressEnter("Press enter to exit", font, 16);
-		
-		gameOver.setFillColor(sf::Color::Red);
-		gameOver.setOrigin(gameOver.getLocalBounds().width / 2.f, gameOver.getLocalBounds().height / 2.f);
-		gameOver.setPosition(window.getSize().x / 2.f, (window.getSize().y / 2.f) - (pressEnter.getLocalBounds().height * 2));
-
-		pressEnter.setOrigin(pressEnter.getLocalBounds().width / 2.f, pressEnter.getLocalBounds().height / 2.f);
-		pressEnter.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
-
-		window.clear();
-		window.draw(gameOver);
-		window.draw(pressEnter);
-		window.display();
+		window.draw(wall);
 	}
+	snake.draw(window);
+	window.draw(player);
+	window.display();
+}
 
-	while (window.isOpen() && window.waitEvent(event))
+if (collided)
+{
+	// end screen
+	bool RetryButtonSelected = true;
+	bool RetryButtonPressed = false;
+	bool ExitButtonSelected = false;
+	bool ExitButtonPressed = false;
+	sf::Text gameOver("GAME OVER", font, 32);
+	sf::Text exitButton("Exit", font, 20);
+	exitButton.setOrigin(exitButton.getLocalBounds().width / 2, exitButton.getLocalBounds().height / 2);
+	exitButton.setPosition(window.getSize().x / 2, window.getSize().y / 2.f);
+	sf::Text retryButton("Retry", font, 20);
+	retryButton.setOrigin(retryButton.getLocalBounds().width / 2, retryButton.getLocalBounds().height / 2);
+	retryButton.setPosition(window.getSize().x / 2, window.getSize().y / 2 - 25.f);
+
+	gameOver.setFillColor(sf::Color::Red);
+	gameOver.setOrigin(gameOver.getLocalBounds().width / 2.f, gameOver.getLocalBounds().height / 2.f);
+	gameOver.setPosition(window.getSize().x / 2, window.getSize().y / 2 - 150.f);
+
+	window.clear();
+	window.draw(gameOver);
+	window.draw(exitButton);
+	window.draw(retryButton);
+	window.display();
+
+	while (window.waitEvent(event))
 	{
-		if (event.type == sf::Event::KeyPressed)
+		if (event.type == sf::Event::Closed)
 		{
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+			window.close();
+		}
+		else if (event.type == sf::Event::KeyPressed)
+		{
+			switch (event.key.code)
+			{
+			case sf::Keyboard::Up:
+			{
+				if (!RetryButtonSelected)
+				{
+					RetryButtonSelected = true;
+					ExitButtonSelected = false;
+					retryButton.setFillColor(sf::Color::Green);
+					exitButton.setFillColor(sf::Color::White);
+					window.clear();
+					window.draw(gameOver);
+					window.draw(exitButton);
+					window.draw(retryButton);
+					window.display();
+				}
+				break;
+			}
+			case sf::Keyboard::W:
+			{
+				if (!RetryButtonSelected)
+				{
+					RetryButtonSelected = true;
+					ExitButtonSelected = false;
+					retryButton.setFillColor(sf::Color::Green);
+					exitButton.setFillColor(sf::Color::White);
+					window.clear();
+					window.draw(gameOver);
+					window.draw(exitButton);
+					window.draw(retryButton);
+					window.display();
+				}
+				break;
+			}
+			case sf::Keyboard::Down:
+			{
+				if (!ExitButtonSelected)
+				{
+					RetryButtonSelected = false;
+					ExitButtonSelected = true;
+					exitButton.setFillColor(sf::Color::Green);
+					retryButton.setFillColor(sf::Color::White);
+					window.clear();
+					window.draw(gameOver);
+					window.draw(exitButton);
+					window.draw(retryButton);
+					window.display();
+				}
+				break;
+			}
+			case sf::Keyboard::S:
+			{
+				if (!ExitButtonSelected)
+				{
+					RetryButtonSelected = false;
+					ExitButtonSelected = true;
+					exitButton.setFillColor(sf::Color::Green);
+					retryButton.setFillColor(sf::Color::White);
+					window.clear();
+					window.draw(gameOver);
+					window.draw(exitButton);
+					window.draw(retryButton);
+					window.display();
+				}
+				break;
+			}
+			case sf::Keyboard::Enter:
+			{
+				RetryButtonPressed = false;
+				ExitButtonPressed = false;
+
+				if (RetryButtonSelected)
+				{
+					RetryButtonPressed = true;
+					window.close();
+					main();
+					break;
+				}
+				else
+				{
+					ExitButtonPressed = true;
+					window.close();
+					break;
+				}
+
+				break;
+			}
+			default:
 			{
 				break;
 			}
+			}
 		}
 	}
-
-	return 0;
 }
 
+
+
+//while (window.isOpen() && window.waitEvent(event))
+//{
+	//if (event.type == sf::Event::KeyPressed)
+	//{
+		//if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+		//{
+			//break;
+		//}
+	//}
+//}
+
+return 0;
+}
+
+/*
+void movePlayer(sf::RectangleShape &shape, sf::Vector2f playerDirection)
+{
+	shape.move(playerDirection);
+}
+*/
 void movePlayer(sf::RectangleShape &shape)
 {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
@@ -291,7 +543,7 @@ void movePlayer(sf::RectangleShape &shape)
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
-		shape.move(MOVE_DISTANCE, 0 );
+		shape.move(MOVE_DISTANCE, 0);
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
